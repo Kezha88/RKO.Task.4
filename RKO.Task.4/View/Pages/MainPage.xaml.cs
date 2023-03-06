40c35d1cede4d5cbe57c0d314bc528aa91c12d3b
Kezha88 <kuku2304@gmail.com>
2023-03-07 02:03:17 +0300

    Добавьте файлы проекта.

---
+++ RKO.Task.4/View/Pages/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using RKO.Task._4.Core;
+using RKO.Task._4.View.Pages.TaskPages;
+using System.Windows;
+using System.Windows.Controls;
+namespace RKO.Task._4.View.Pages
+{
+    public partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
